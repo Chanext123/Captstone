5c2a3714ab10baa36c23ef1747bfa462fb08cfd5
Christian Misa <102141983+Chanext123@users.noreply.github.com>
2023-07-12 12:10:51 +0800

    for the capstone july 12

---
+++ Capstone/Favio-main/Favio-main/Capstone_Website/contact.js
@@ -0,0 +1,24 @@
+var typed = new Typed('.typing1', options);
+//   var typed = new Typed('.typing2', options);
+
+ScrollOut({
+    targets: '.img, .aboutText , .box, div.left, div.right, .card'
+})
+ScrollOut({
+    targets: '.header',
+    offset: 50
+})
+
+var options = {
+    strings: ['Favio Valentino Jasso'],
+    typeSpeed: 300,
+    backSpeed: 50,
+    loop: false
+};
+
+
+
+
+
+
+
